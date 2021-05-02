1d4559a909060919058da213908bd0926f8c9ac0
Marcos Vinicius <marcosvinicius.udia1256@gmail.com>
2021-05-02 18:05:17 -0300

    finish project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Container, AppBar, Grow, Typography, Grid } from '@material-ui/core';
+import Student from './components/showStudent/showStudent.js';
+import Create from './components/createStudent/createStudent.js';
+import useStyles from './styles';
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container maxWidth="lg" >
+        <AppBar className={classes.appBar} position='static' color='inherit'>
+          <Typography className={classes.heading} variant='h2' align='center'>
+            Students Create & Show
+          </Typography>
+        </AppBar>
+
+        <Grow in>
+          <Container>
+            <Grid container justify="space-between" alignItems='stretch'>
+              <Grid item xs={12} sm={7}>
+                <AppBar className={classes.appBar} position='static' color='inherit' >
+                  <Student />
+                </AppBar>
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <AppBar className={classes.appBar} position='static' color='inherit' >
+                  <Create />
+                </AppBar>
+              </Grid>
+
+            </Grid>
+          </Container>
+        </Grow>
+      </Container>
     </div>
   );
 }
